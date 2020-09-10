5dc996ac5ec929a534a3da6ecb3550aa6ae1c16a
mikedee95 <70868084+mikedee95@users.noreply.github.com>
2020-09-10 14:04:49 +0700

    Add files via upload

---
+++ area.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt('Enter the Width');
+inputHeight = prompt('Enter the Height');
+let Width = Number(inputWidth);
+let Height = Number(inputHeight);
+let area = Width * Height;
+document.write('The area is :'+ area);
